3f383687e04f0dd6a4998ce1f83b02d9125244ae
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-27 11:43:07 +0300

    Add Discount value over product image

--- src/components/productCard/ProductsCard.tsx
+++ src/components/productCard/ProductsCard.tsx
@@ -1,52 +1,72 @@
 import * as React from "react";
-import AspectRatio from "@mui/joy/AspectRatio";
 import Button from "@mui/joy/Button";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
 import CardOverflow from "@mui/joy/CardOverflow";
-import Chip from "@mui/joy/Chip";
 import Link from "@mui/joy/Link";
 import Typography from "@mui/joy/Typography";
-import { Product } from "./types";
-import { Box, Rating } from "@mui/material";
-import { purple, red, grey } from "@mui/material/colors";
+import { Product } from "../types";
+import { Box, CardMedia, Rating } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { CardCover } from "@mui/joy";
+import Chip from "./Chip";
 
 export default function ProductCard({ product }: { product: Product }) {
   const [value, setValue] = React.useState<number | null>(product.rating);
 
   return (
     <Card
-      sx={{ width: 320, maxWidth: "100%", boxShadow: "lg", margin: "auto" }}
+      sx={{
+        width: 320,
+        height: 400,
+        maxWidth: "100%",
+        boxShadow: "lg",
+        margin: "auto",
+      }}
+      variant="soft"
     >
       <CardOverflow>
-        <AspectRatio sx={{ minWidth: 200 }}>
-          <img src={product.image} loading="lazy" alt="" />
-        </AspectRatio>
+        <CardCover>
+          <CardMedia
+            component="img"
+            height="240"
+            src="https://images.unsplash.com/photo-1502657877623-f66bf489d236?auto=format&fit=crop&w=800"
+            loading="lazy"
+            alt=""
+          />
+        </CardCover>
+        <CardContent>
+          <Typography
+            level="body-lg"
+            fontWeight="lg"
+            textColor="#fff"
+            mt={{ xs: 0 }}
+            mb={{ xs: 24 }}
+          >
+            <Chip label={product.discount} />
+          </Typography>
+        </CardContent>
       </CardOverflow>
       <CardContent>
-        {/* <Typography level="body-xs">{product.name}</Typography> */}
         <Link
           href="#product-card"
           fontWeight="md"
           color="neutral"
           textColor="text.primary"
+          sx={{ mt: 2.5 }}
           overlay
         >
           {product.name}
         </Link>
 
         <div className="flex gap-1 items-center">
-          <Box
-            sx={{
-              "& > legend": { mt: 2 },
-            }}
-          >
+          <Box>
             <Rating
               name="simple-controlled"
               value={value}
               sx={{ mt: 0.75 }}
               onChange={(event, newValue) => {
-                setValue(newValue);
+                setValue(newValue || 0);
               }}
             />
           </Box>
@@ -59,9 +79,6 @@ export default function ProductCard({ product }: { product: Product }) {
           level="title-lg"
           sx={{ mt: 1, fontWeight: "xl", color: "#C24B5A" }}
           endDecorator={
-            // <Chip component="span" size="sm" variant="soft" color="success">
-            //   Lowest price
-            // </Chip>
             <Typography
               level="body-md"
               sx={{ textDecoration: "line-through", color: grey[500] }}
@@ -73,11 +90,6 @@ export default function ProductCard({ product }: { product: Product }) {
           ${product.price}
         </Typography>
       </CardContent>
-      <CardOverflow>
-        <Button variant="solid" color="danger" size="lg">
-          Add to cart
-        </Button>
-      </CardOverflow>
     </Card>
   );
 }
