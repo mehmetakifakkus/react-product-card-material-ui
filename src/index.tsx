c77a92ea430bebd5b52278951464f8465c071e22
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-28 18:07:00 +0300

    Add provider for redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+// components
 import App from "./App";
 import Navbar from "./components/navbar/Navbar";
+
+// state management
+import { Provider } from "react-redux";
+import store from "./redux/createStore";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Navbar />
-    <div className="pb-20 pt-28">
-      <App />
-    </div>
+    <Provider store={store}>
+      <Navbar />
+      <div className="pb-20 pt-28">
+        <App />
+      </div>
+    </Provider>
   </React.StrictMode>
 );
