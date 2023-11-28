cc7aa4f5bc5ae06a0556fafb47778491dab13428
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-28 23:21:11 +0300

    Remove redundant api folder

--- src/apis/listProducts.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const listProducts = async () => {
-  const { data } = await axios.get(
-    "https://linkedin-cv-crawler.beta-limited.workers.dev/interview/products"
-  );
-  return data;
-}
--- src/apis/searchProducts.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const searchProducts = async (searchKey: string) => {
-  const { data } = await axios.get(
-    `https://linkedin-cv-crawler.beta-limited.workers.dev/interview/search?name=${searchKey}`
-  );
-  return data;
-}
