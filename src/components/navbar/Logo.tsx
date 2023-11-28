b5a45083e29b4a3490b7a75a5fad34d878ee2f92
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-28 22:54:36 +0300

    Change logo style

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -16,7 +16,7 @@ function Logo() {
         alt="Betalimited logo"
         className="cursor-pointer filter hover:brightness-125 transition duration-150 ease-in-out h-12 w-12 sm:h-10 md:w-10 rounded-md"
       />
-      <span className="hidden md:block text-base sm:text-xl md:text-2xl font-semibold text-neutral-800">
+      <span className="hidden lg:block text-base sm:text-xl md:text-2xl font-semibold text-neutral-800">
         Beta Limited
       </span>
     </div>
