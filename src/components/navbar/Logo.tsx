208f6a8ffc30884021f0229c9fa97219bd1ea0b7
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-28 18:15:01 +0300

    Get all products when logo pressed

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,13 +1,20 @@
+import { useDispatch } from "react-redux";
+import { postTypes } from "../../redux/actionTypes";
+
 function Logo() {
+  const dispatch = useDispatch();
+
   return (
-    <div className="flex gap-2 items-end cursor-pointer">
+    <div
+      className="flex gap-2 items-end cursor-pointer"
+      onClick={() => {
+        dispatch({ type: postTypes.GET_PRODUCTS });
+      }}
+    >
       <img
         src="/logo.jpeg"
-        alt="betalimited logo"
+        alt="Betalimited logo"
         className="cursor-pointer filter hover:brightness-125 transition duration-150 ease-in-out h-12 w-12 sm:h-10 md:w-10 rounded-md"
-        onClick={() => {
-          // router.push("/");
-        }}
       />
       <span className="hidden md:block text-base sm:text-xl md:text-2xl font-semibold text-neutral-800">
         Beta Limited
