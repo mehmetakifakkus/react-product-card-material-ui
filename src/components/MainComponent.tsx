5373d668207db6f8c68d787823e317962ba8e323
Mehmet Akif AKKUŞ <mehmetakifakkus@gmail.com>
2023-11-28 22:34:06 +0300

    Fetch products and carts

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,40 +1,45 @@
-import React from "react";
-import { listProducts } from "../apis/listProducts";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
 import { Grid } from "@mui/material";
 import ProductCard from "./productCard/ProductCard";
 import { Product } from "./types";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { postTypes } from "../redux/actionTypes";
 
-type Props = {};
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import EmptyState from "./EmptyState";
 
-export default function MainComponent({}: Props) {
-  const queryClient = useQueryClient();
+export default function MainComponent() {
+  const [parent] = useAutoAnimate({ duration: 300 });
+  const dispatch = useDispatch();
 
-  // Queries
-  const query = useQuery({ queryKey: ["todos"], queryFn: listProducts });
-  const products: Product[] = query.data;
+  useEffect(() => {
+    dispatch({ type: postTypes.GET_PRODUCTS });
+    dispatch({ type: postTypes.GET_CART });
+  }, [dispatch]);
 
-  // Mutations
-  // const mutation = useMutation({
-  //   mutationFn: postTodo,
-  //   onSuccess: () => {
-  //     // Invalidate and refetch
-  //     queryClient.invalidateQueries({ queryKey: ["todos"] });
-  //   },
-  // });
+  const products: Product[] = useSelector(
+    (state: any) => state.product.products
+  );
 
   return (
     <div>
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
+        rowSpacing={{ xs: 6 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
+        ref={parent}
       >
-        {products?.map((product, index) => (
-          <Grid item xs={4} sm={4} md={4} key={index}>
-            <ProductCard product={product} />
-          </Grid>
-        ))}
+        {products?.length === 0 ? (
+          <EmptyState />
+        ) : (
+          products?.map((product, index) => (
+            <Grid item xs={4} sm={4} md={4} key={index}>
+              <ProductCard product={product} />
+            </Grid>
+          ))
+        )}
       </Grid>
     </div>
   );
